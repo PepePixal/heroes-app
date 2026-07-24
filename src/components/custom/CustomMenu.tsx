6e50b547177db2a62ccc21d46ab022146973a0e3
PepePixal <pepepixal@gmail.com>
2026-07-24 18:11:06 +0200

    Migracion-de-BaseUI-a-RadixUI

--- src/components/custom/CustomMenu.tsx
+++ src/components/custom/CustomMenu.tsx
@@ -1,7 +1,8 @@
 
+import { NavigationMenuItem, NavigationMenuLink  } from "@radix-ui/react-navigation-menu";
+import { Link, useLocation } from "react-router";
+import {  NavigationMenu, NavigationMenuList } from "../ui/navigation-menu";
 import { cn } from "@/lib/utils";
-import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, /*navigationMenuTriggerStyle*/} from "../ui/navigation-menu"
-import { Link, useLocation } from "react-router"
 
 export const CustomMenu = () => {
 
@@ -12,56 +13,35 @@ export const CustomMenu = () => {
     // func. que recibe un path y lo compara con el path de la URL actual (pathname), retorna true o false
     const isActive = (path: string) => {
         return pathname === path;
-    }
+    };
 
     return (
+         <NavigationMenu className="mb-3">
+             <NavigationMenuList>
+                 {/* Home */}
+                 <NavigationMenuItem>
+                     <NavigationMenuLink
+                        asChild
+                        // Para agregar clases (de Tailwind) dinamicas, se aconseja usar la func. helper cn().
+                        // Si el path enviado a isActive() es el path actual, isActive retorna (true),
+	                    // entoces && agrega la clase "bg-slate-200"
+                        className={cn(isActive('/') && 'bg-slate-300', 'rounded-md p-2 mr-3')}
+                     >
+                        <Link to="/">Inicio</Link>
+                     </NavigationMenuLink>
+                 </NavigationMenuItem>
 
-        <NavigationMenu>
-            <NavigationMenuList>
-                {/* Home */}
-                <NavigationMenuItem>
+                 {/* Search */}
+                 <NavigationMenuItem>
                     <NavigationMenuLink
-                        render={<Link to="/" />}
-                        // Para agregar clases (de Tailwind) dinamicas usar la func. helper cn().
-                        // Si el path enviado a isActive() es el path actual (true), agrega las clases
-                        className={ cn( isActive('/') && "bg-slate-200", "rounded-md p-2 mr-3") }
-                        //className={navigationMenuTriggerStyle()}
+                        asChild
+                        className={cn(isActive('/search') && 'bg-slate-300', 'rounded-md p-2 mr-3')}
                     >
-                        Inicio
+                        <Link to="/search">Buscar SuperHéroes</Link>
                     </NavigationMenuLink>
-                </NavigationMenuItem>
-
-                {/* Search */}
-                <NavigationMenuItem>
-                    <NavigationMenuLink
-                        render={<Link to="/search" />}
-                        className={ cn( isActive('/search') && "bg-slate-200", "rounded-md p-2 ml-3") }
-                        //className={navigationMenuTriggerStyle()}
-                    >
-                        Buscar Superhéroes
-                    </NavigationMenuLink>
-                </NavigationMenuItem>
-            </NavigationMenuList>
-        </NavigationMenu>
-
-
-        // Menú
-        //  <NavigationMenu>
-        //      <NavigationMenuList>
-        //          {/* Home */}
-        //          <NavigationMenuItem>
-        //              <NavigationMenuLink className="bg-slate-200 rounded-md p-2 mr-3">
-        //                  <Link to="/">Inicio</Link>
-        //              </NavigationMenuLink>
-        //          </NavigationMenuItem>
-        //          {/* Search */}
-        //          <NavigationMenuItem>
-        //              <NavigationMenuLink className="bg-slate-200 rounded-md p-2 mr-3">
-        //                  <Link to="/search">Buscar SuperHéroes</Link>
-        //              </NavigationMenuLink>
-        //          </NavigationMenuItem>
-        //      </NavigationMenuList>
-        //  </NavigationMenu>
+                 </NavigationMenuItem>
+             </NavigationMenuList>
+         </NavigationMenu>
         
     );
 
